2d80788382cf4bc46bf7887bd221c6865bd3620c
nawalkanabee <nawalkanabee@gmail.com>
2022-10-25 16:11:17 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomemessage = 'welcomemessage !' + currentUser;
+const shortGreeting ='shortGreeting!';
+const  welcomeMessage = "Welcome to Flatbook, ":
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser}`;
+'Edsger Dijkstra'.slice();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
